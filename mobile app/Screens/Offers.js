838cc653b97cbf7e15f840759f3b44d325b9ba20
asemqaffaf <asem@qaffaf.com>
2019-11-11 21:23:20 +0200

    nav icon

--- mobile app/Screens/Offers.js
+++ mobile app/Screens/Offers.js
@@ -1,17 +1,40 @@
 import React from 'react'
 import {View , Text} from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
+import Profile from './Profile'
+import Home from './Home'
+import {
+    createStackNavigator,
+    // createBottomTabNavigator,
+    createNavigationContainer,
+createMaterialTopTabNavigator
+  } from "react-navigation";
+// export default function Offers () {
+//         return (
+//             <View style={{marginTop:100}}>
+//                 <Text>Offers Page</Text>
+//                 <Ionicons name="md-home" size={32} color="green" />
+//             </View>
+//         )
+//     }
 
-export default function Offers () {
-        return (
-            <View style={{marginTop:100}}>
-                <Text>Offers Page</Text>
-                <Ionicons name="md-home" size={32} color="green" />
-            </View>
-        )
-    }
 
-    Offers.navigationOptions = {
-    title: 'Offers',
-  };
-  
+const SellerStack = createStackNavigator({
+    Profile
+})
+SellerStack.navigationOptions={
+    title : 'Seller'
+}
+const BuyerStack = createStackNavigator({
+    Home
+})
+BuyerStack.navigationOptions={
+    title : 'Buyer'
+}
+const topNav = createMaterialTopTabNavigator({
+    SellerStack,
+    BuyerStack
+})
+
+const container =  createNavigationContainer(topNav)
+export default (container)
