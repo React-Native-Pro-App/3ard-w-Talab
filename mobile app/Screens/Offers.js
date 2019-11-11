bef777201bb0a5d934a8062c371b5881bd8df107
asemqaffaf <asem@qaffaf.com>
2019-11-11 21:42:08 +0200

    offers changes

--- mobile app/Screens/Offers.js
+++ mobile app/Screens/Offers.js
@@ -1,40 +1,32 @@
-import React from 'react'
-import {View , Text} from 'react-native'
-import { Ionicons } from '@expo/vector-icons';
-import Profile from './Profile'
-import Home from './Home'
+import BuyerScreen from './Offers/BuyerScreen'
+import SellerScreen from './Offers/SellerScreen'
 import {
     createStackNavigator,
-    // createBottomTabNavigator,
-    createNavigationContainer,
-createMaterialTopTabNavigator
-  } from "react-navigation";
-// export default function Offers () {
-//         return (
-//             <View style={{marginTop:100}}>
-//                 <Text>Offers Page</Text>
-//                 <Ionicons name="md-home" size={32} color="green" />
-//             </View>
-//         )
-//     }
+    createMaterialTopTabNavigator,
+    createAppContainer
+} from "react-navigation";
 
 
 const SellerStack = createStackNavigator({
-    Profile
+    SellerScreen
 })
-SellerStack.navigationOptions={
-    title : 'Seller'
+SellerStack.navigationOptions = {
+    title: 'Seller'
 }
 const BuyerStack = createStackNavigator({
-    Home
+    BuyerScreen
 })
-BuyerStack.navigationOptions={
-    title : 'Buyer'
+BuyerStack.navigationOptions = {
+    title: 'Buyer'
 }
 const topNav = createMaterialTopTabNavigator({
     SellerStack,
     BuyerStack
 })
 
-const container =  createNavigationContainer(topNav)
+
+const container = createAppContainer(topNav)
+container.navigationOptions={
+    title : 'Offers'
+}
 export default (container)
---
+++ mobile app/Screens/Offers/SellerScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import {View , Text } from 'react-native'
+export default class SellerScreen extends Component {
+  render() {
+    return (
+      <>
+        <View style={{margin:100}}>
+            <Text>SellerScreen</Text>
+        </View>
+      </>
+    )
+  }
+}
